9d4e7d41fdc6eef4cc6e27935662a8127bd8f5d0
daniel <schmaldaniel@gmail.com>
2017-03-25 15:52:53 -0700

    modified wrapper and some ansible yaml

--- wrapper.sh
+++ wrapper.sh
@@ -1,49 +1,87 @@
 #!/bin/bash
 
+# Usually I prefer to write those wraper in python 
+# since I am not familiar with ansible and especially 
+# with ansible API I used bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+new_deployment=0
+
 function usage {
     echo "Sript usage:"
     echo "------------"
     echo ""
-    echo "./wrapper.sh -a/--app {app name to be installed - gify-panda/counter-panda}"
+    echo "./wrapper.sh -n/--new-deployment -a/--app {app name to be installed - gify-panda/counter-panda}"
     echo ""
     echo "Examples:"
     echo "--------"
     echo ""
-    echo "install both apps:"
+    echo "install both apps on existing base server:"
     echo "./wapper.sh"
-    echo " install single app"
+    echo "install single app on existing base server"
     echo "./wapper.sh -a gify-panda"
+    echo "deploy base server from scratch without -a parameter will install both"
+    echo "./wapper.sh -n -a gify-panda"
     exit
 
 }
 
 function run_playbook {
-    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    # Run ansible playbook on existing base server
     cd $DIR
     if [ ! -z $APP ]; then
         ansible-playbook -i dev/hosts base.yml --tags $APP
     else
         ansible-playbook -i dev/hosts base.yml
     fi
-    echo $DIR
 }
 
-while [[ $# -gt 1 ]]
+function destroy_base {
+    # Removing existing base server
+    vagrant destroy -f base
+}
+
+function create_vagrantfile {
+    # Creating vagrantfile based on the apps that should be run in the base server
+    cd $DIR
+    cp -fp Vagrantfile.template Vagrantfile
+    if [ -v $APP ]; then
+        APP='["gify-panda", "counter-panda"]'
+    fi
+    sed -i s/{{TAGS}}/"'$APP'"/g Vagrantfile
+}
+
+function create_base {
+    # Creating the base server
+    vagrant up base
+}
+
+
+
+while [[ $# -gt 0 ]]
 do
 key="$1"
 case $key in
     -h|--help)
     usage
-    shift # past argument
     ;;
     -a|--app)
     APP="$2"
-    shift # past argument
+    shift 
+    ;;
+    -n|--new-deployment)
+    new_deployment=1
     ;;
     *)
     ;;
 esac
-shift # past argument or value
+shift 
 done
 
-run_playbook
+if [ "$new_deployment" -eq 1 ]; then
+    destroy_base
+    create_vagrantfile
+    create_base
+else
+    run_playbook
+fi
